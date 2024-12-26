a638415ae45129546c8d16f20cf0ed9fe4adc3d0
SaifUllahWebDev <saifullahwebdev@gmail.com>
2024-12-25 15:06:13 -1100

    commit
    done

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+var num1 = +prompt("Enter Your First Number:");
+var num2 = +prompt("Enter Your Second Number:");
+
+var add = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var divi = num1 / num2;
+var modul = num1 % num2;
+
+alert("Addition: " + add +
+    "\nSubtract: " + sub +
+    "\nMultiple: " + mul +
+    "\nDivision: " + divi +
+    "\nModulus: " + modul);
